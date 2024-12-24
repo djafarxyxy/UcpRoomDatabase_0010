9b7c881ffd8947321d613ac263e77fa5cbc8c676
djafarxyxy <farhanch12344@gmail.com>
2024-12-24 14:26:43 +0700

    Menambahkan entitas Jadwal

---
+++ app/src/main/java/com/example/ucp2/Data/entity/Jadwal.kt
@@ -0,0 +1,16 @@
+package com.example.ucp2.Data.entity
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Jadwal")
+data class Jadwal(
+    @PrimaryKey
+    val id: String,
+    val namaDokter: String,
+    val namaPasien: String,
+    val noTelpon: String,
+    val tanggalKonsultasi: String,
+    val status: String
+)
